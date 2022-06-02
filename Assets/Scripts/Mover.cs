a148164106a8ac24813bf591a3b6b488617b4c06
Сергей Буркун <GizmoG2@live.ru>
2022-06-02 17:44:02 +0300

    Обновлена версия юнити. Движение по плоскости привязяно к клавишам.

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -4,9 +4,9 @@ using UnityEngine;
 
 public class Mover : MonoBehaviour
 {
-    [SerializeField] float xValue = 0.01f;
-    [SerializeField] float yValue = 0.01f;
-    [SerializeField] float zValue = 0.01f;
+    //[SerializeField] float xValue = 0.01f;
+    [SerializeField] float yValue = 0.00f;
+    //[SerializeField] float zValue = 0f;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,6 +16,8 @@ public class Mover : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        float xValue = Input.GetAxis("Horizontal") * 2 * Time.deltaTime;
+        float zValue = Input.GetAxis("Vertical") * 2 * Time.deltaTime;
         transform.Translate(xValue, yValue, zValue);
     }
 }
