37d3d1cd6973fc0e6e5b017dc65e40bbaa182399
Сергей Буркун <GizmoG2@live.ru>
2022-06-05 21:13:04 +0300

    Добавлена переменная для скорости. Добавлена синемашина и виртуальная камера.

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -4,8 +4,10 @@ using UnityEngine;
 
 public class Mover : MonoBehaviour
 {
+
+    [SerializeField] int moveSpeed;
+    //[SerializeField] float yValue = 0.00f;
     //[SerializeField] float xValue = 0.01f;
-    [SerializeField] float yValue = 0.00f;
     //[SerializeField] float zValue = 0f;
     // Start is called before the first frame update
     void Start()
@@ -16,8 +18,8 @@ public class Mover : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float xValue = Input.GetAxis("Horizontal") * 2 * Time.deltaTime;
-        float zValue = Input.GetAxis("Vertical") * 2 * Time.deltaTime;
-        transform.Translate(xValue, yValue, zValue);
+        float xValue = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
+        float zValue = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
+        transform.Translate(xValue, 0, zValue);
     }
 }
